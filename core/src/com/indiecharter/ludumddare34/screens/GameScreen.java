de88ca97df824d9ca12b1199f0f94343f114cfce
Alexander <sorrer@gmail.com>
2015-12-11 22:39:21 -0500

    Fixed switching

--- core/src/com/indiecharter/ludumddare34/screens/GameScreen.java
+++ core/src/com/indiecharter/ludumddare34/screens/GameScreen.java
@@ -2,6 +2,7 @@ package com.indiecharter.ludumddare34.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.indiecharter.ludumddare34.CoreGame;
@@ -15,6 +16,7 @@ public class GameScreen implements Screen{
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		this.game = game;
+		System.out.println("Moved to gamescreen");
 	}
 	
 	@Override
@@ -26,6 +28,8 @@ public class GameScreen implements Screen{
 	public void render(float delta) {
 		update();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 	}
 	
 	public void update(){

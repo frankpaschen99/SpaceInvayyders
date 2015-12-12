93b7468f9a1c0ee54cc6912b7d84d7db2c361040
Alexander <sorrer@gmail.com>
2015-12-12 12:49:31 -0500

    Added textures

--- core/src/com/indiecharter/ludumddare34/gui/ProgressBar.java
+++ core/src/com/indiecharter/ludumddare34/gui/ProgressBar.java
@@ -1,5 +1,8 @@
 package com.indiecharter.ludumddare34.gui;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public class ProgressBar extends Gui{
 	//TODO: Created progress bar that can be use vertical or horizontal
+	Texture barOutline;
 }

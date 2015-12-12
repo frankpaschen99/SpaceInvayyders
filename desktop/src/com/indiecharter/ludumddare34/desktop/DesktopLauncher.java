23afb23fa9d9f992ce2a2d0039c672a1880ca99f
unknown <sorrer@gmail.com>
2015-12-11 21:10:46 -0500

    Ayy lamo first commit pro

---
+++ desktop/src/com/indiecharter/ludumddare34/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.indiecharter.ludumddare34.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.indiecharter.ludumddare34.CoreGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoreGame(), config);
+	}
+}

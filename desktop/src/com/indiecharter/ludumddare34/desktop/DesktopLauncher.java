a6103ae0f4584cd6dd4fc21fadfe656e699a902f
Alexander <sorrer@gmail.com>
2015-12-11 21:22:52 -0500

    Desktop Launcher changes

--- desktop/src/com/indiecharter/ludumddare34/desktop/DesktopLauncher.java
+++ desktop/src/com/indiecharter/ludumddare34/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.indiecharter.ludumddare34.CoreGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		config.resizable = false;
+		config.vSyncEnabled = true;
+		config.title = "";
 		new LwjglApplication(new CoreGame(), config);
 	}
 }
